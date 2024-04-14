39d8683e45ec34f814fcdcc844896fd030da9a4a
Łukasz Kużyński <lukasz.kuzynski@gmail.com>
2024-04-14 22:22:52 +0200

    Event more explicit keyid rules

--- src/KeyId.ts
+++ src/KeyId.ts
@@ -4,6 +4,7 @@ export const KeyIdSchema = z
 	.string()
 	.trim()
 	.min(1, "Key ID Cannot be empty")
+	.max(255, "Key ID is too long. Max length is 255 characters")
 	.regex(/^[a-z0-9_-]+$/i, "Key ID can contain only letters, numbers, underscores and dashes");
 export type KeyId = z.infer<typeof KeyIdSchema>;
 export type KeyIdInput = z.input<typeof KeyIdSchema>;
